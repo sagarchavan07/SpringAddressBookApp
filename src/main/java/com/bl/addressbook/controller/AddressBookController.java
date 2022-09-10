7762a3e0c8689790e7fa29c1c2f0717c326291a5
sagarchavan07 <chavansagar96044@gmail.com>
2022-09-10 17:14:14 +0530

    setup Controller,Service, DTO, model and handeled GET call using service to give wellcome message

--- src/main/java/com/bl/addressbook/controller/AddressBookController.java
+++ src/main/java/com/bl/addressbook/controller/AddressBookController.java
@@ -1,5 +1,9 @@
 package com.bl.addressbook.controller;
 
+import com.bl.addressbook.dto.ResponseDTO;
+import com.bl.addressbook.service.AddressBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/addressbook")
 public class AddressBookController {
 
+    @Autowired
+    AddressBookService addressBookService;
+
     @GetMapping(value = {"","/"})
-    public String getAddressBook(){
-        return "GET call success";
+    public ResponseEntity<ResponseDTO> hello(){
+        return addressBookService.welcomeMessage();
     }
 }
